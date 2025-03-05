0b127aac686897b00259367129fcf4269820c57c
Vladimir <vova2066@yandex.ru>
2025-03-05 18:33:38 +0300

    deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/tour-operator-auth-react/",
 
   server: {
     host: true, // или '0.0.0.0'
